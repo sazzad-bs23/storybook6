63008362ca43899b2ffa364d0a3c175c53d7d674
Md. Sazzad Hossain <sazzad.hossain@brainstation-23.com>
2021-10-12 19:48:13 +0600

    Adds Bootstrap & updates structures

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import "../src/styles/styles.scss";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
---
+++ src/components/molecules/Subscription/Subscription.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Primary } from "../Button/Button.stories";
+import { Large } from "../Input/Input.stories";
+
+export default {
+    title: 'Molecules/Subscription'
+}
+
+export const PrimarySubscription = () => (
+    <>
+        <Large />
+        <Primary />
+    </>
+)
